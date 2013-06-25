6f1eca84d1f7ca15820667ad23cd3acae5887d33
birendra <birendra@birendra.(none)>
2013-06-25 23:20:54 +0545

    field import fixed

---
+++ test/unit/com/taskin/BootstrapServiceTests.groovy
@@ -0,0 +1,30 @@
+package com.taskin
+
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+
+@TestFor(BootstrapService)
+@Mock([User,Role,UserRole,Project])
+class BootstrapServiceTests {
+    
+    void testDoBootStrapRoles() {        
+        service.doBootStrapRoles()
+        assertTrue 'role should have been added', Role.count() > 0
+        assertNotNull 'admin role should have been created',Role.findByAuthority('ROLE_ADMIN')
+        assertNotNull 'user role should have been created',Role.findByAuthority('ROLE_USER')
+    }
+    
+    void testDoBootStrapUsers(){
+        //do nothing when encodePassword is called
+        User.metaClass.encodePassword = {->}
+        service.doBootStrapUsers()
+        assertTrue 'user should have been added', User.count() > 0        
+        assertNotNull 'admin should have been created',User.findByUsername('admin@henyo.com')
+        assertNotNull 'user should have been created',User.findByUsername('user@henyo.com')
+    }
+    
+    void testDoBootStrapProjects(){
+        service.doBootStrapProjects()
+    }
+    
+}
